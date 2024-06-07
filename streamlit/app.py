fde3a646e34c7524c01913af7428bbbe697204fa
emmanuelniasse <emmanuelniasse@gmail.com>
2024-06-07 12:37:09 +0200

    wip: streamli

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,1 +1,19 @@
-print("L'application Streamlit fonctionne correctement !")
+import streamlit as st
+
+st.write("hello world!")
+
+# from socketIO_client_nexus import SocketIO
+
+# st.title("Données en temps réel depuis Node.js")
+
+# # Connexion au serveur Node.js via Socket.IO
+# socketIO = SocketIO("localhost", 3000)
+
+# # Fonction pour afficher les données reçues
+# def on_station_update(data):
+#     st.write(f"Nouvelle mise à jour de station : {data}")
+
+# # Écoute des événements "station_update"
+# socketIO.on("station_update", on_station_update)
+
+# st.write("En attente de mises à jour...")
